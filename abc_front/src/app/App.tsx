6f0e52d34902b79a6b5e510479228f8c425a4d1b
rlarjsgml1 <qhdudrjsgml@gmail.com>
2026-07-12 12:39:28 +0900

    chore: App.tsx 중복 전역 CSS import 제거
    main.tsx가 index.css를 통해 variables/base/layout.css를 이미
    불러오는데 App.tsx가 동일한 3개 파일을 다시 import하고 있어
    번들에 CSS가 중복 포함되고 있었다.

--- abc_front/src/app/App.tsx
+++ abc_front/src/app/App.tsx
@@ -1,8 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
-import '../styles/variables.css';
-import '../styles/base.css';
-import '../styles/layout.css';
 
 export default function App() {
   return <RouterProvider router={router} />;
